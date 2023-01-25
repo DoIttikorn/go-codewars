c0ab67c9318ea795ce0fb89159fee7e57f371e00
ittikorn chawkamud <ittikorn.chawkamud@scbtechx.io>
2023-01-25 11:50:13 +0700

    remove test case

--- codewars/maximumLengthDifference/main_test.go
+++ codewars/maximumLengthDifference/main_test.go
@@ -13,14 +13,6 @@ func TestMxDifLg(t *testing.T) {
 		}
 	})
 
-	// t.Run("Test 2", func(t *testing.T) {
-	// 	want := -1
-	// 	got := MxDifLg([]string{"it", "wkppv", "ixoyx", "3452", "zzzzzzzz zz"}, []string{"x", "3452", "zzzzzzzz zz"})
-	// 	if want != got {
-	// 		t.Errorf("want %v got %v", want, got)
-	// 	}
-	// })
-
 	t.Run("Test 3", func(t *testing.T) {
 		want := 10
 		got := MxDifLg([]string{"ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"}, []string{"bbbaaayddqbbrrrv"})
